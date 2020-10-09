28f1174f359bde122b238be219af26e22edfdd89
Haseeb Zia <haseebzia99@outlook.com>
2020-10-09 20:29:18 +0100

    refactored code

--- src/GreenLoginForm.js
+++ src/GreenLoginForm.js
@@ -17,48 +17,55 @@ const handleFormErrors = (values) => {
   }
   return errors;
 }
+
+const handleFormSubmission = (values, setSubmitting) => {
+  setTimeout(() => {
+   alert(JSON.stringify(values, null, 2));
+   setSubmitting(false);
+  }, 400);
+}
+
+const renderForm = (isSubmitting) => {
+  return(
+    <div className="form-content">
+
+      <img src={logo} className="green-logo" alt="green-logo" />
+
+      <div className="form-headers">
+        <h2>Example login screen</h2>
+        <h3>Getting started with Green.</h3>
+      </div>
+
+      <Form>
+        <div className="email-field">
+          <label htmlFor="email" id="email-label">Email Address</label>
+          <Field type="email" name="email" id="email-input" maxLength="40"/>
+          <ErrorMessage name="email" component="div" />
+        </div>
+
+        <div className="checkbox-field">
+          <Field type="checkbox" name="checkbox" id="checkbox-input"/>
+          <label htmlFor="checkbox" id="checkbox-label">Remember this device</label>
+        </div>
+
+        <div>
+          <button type="submit" disabled={isSubmitting} id="signin-btn" checked="value">
+            Sign In
+          </button>
+        </div>
+      </Form>
+    </div>
+  )
+}
+
 const GreenLoginForm = () => (
   <div className='form-container'>
    <Formik
      initialValues={{ email: ''}}
      validate={values => handleFormErrors(values)}
-     onSubmit={(values, { setSubmitting }) => {
-       setTimeout(() => {
-         alert(JSON.stringify(values, null, 2));
-         setSubmitting(false);
-       }, 400);
-     }}
+     onSubmit={(values, { setSubmitting }) => handleFormSubmission(values, setSubmitting)}
    >
-     {({ isSubmitting }) => (
-      <div className="form-content">
-
-        <img src={logo} className="green-logo" alt="green-logo" />
-
-        <div className="form-headers">
-          <h2>Example login screen</h2>
-          <h3>Getting started with Green.</h3>
-        </div>
-
-        <Form>
-          <div className="email-field">
-            <label htmlFor="email" id="email-label">Email Address</label>
-            <Field type="email" name="email" id="email-input" maxLength="40"/>
-            <ErrorMessage name="email" component="div" />
-          </div>
-
-          <div className="checkbox-field">
-            <Field type="checkbox" name="checkbox" id="checkbox-input"/>
-            <label htmlFor="checkbox" id="checkbox-label">Remember this device</label>
-          </div>
-
-          <div>
-            <button type="submit" disabled={isSubmitting} id="signin-btn" checked="value">
-              Sign In
-            </button>
-          </div>
-        </Form>
-      </div>
-     )}
+     {({isSubmitting}) => renderForm(isSubmitting)}
    </Formik>
   </div>
 );
